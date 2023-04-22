42fc6fa60a68c58abf27f6ac6f3cb1ad8c989828
Hamid Yaftian <hamid@deriv.com>
2023-04-22 13:24:47 +0800

    add analytics

--- src/pages/App/App.tsx
+++ src/pages/App/App.tsx
@@ -1,8 +1,18 @@
+import { useEffect } from 'react';
+import ReactGA from 'react-ga';
 import Header from '@/components/header';
 import Footer from '@/components/footer';
 import Playground from '@/components/playground';
 
 const App = () => {
+    useEffect(() => {
+        // Initialize Google Analytics
+        ReactGA.initialize('G-V2W8PRVXZ7');
+        if (typeof window !== 'undefined') {
+            ReactGA.pageview(window.location.pathname + window.location.search);
+        }
+    }, []);
+
     return (
         <div className="flex flex-col justify-between h-screen">
             <Header />
